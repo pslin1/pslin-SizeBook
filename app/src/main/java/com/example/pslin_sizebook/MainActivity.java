e2bae42ecf65faf3ee9c173a388836873312cc0b
pslin1 <pslin@ualberta.ca>
2017-01-31 15:08:18 -0700

    crashes on start, nullpointerexception, something to do with adapter

--- app/src/main/java/com/example/pslin_sizebook/MainActivity.java
+++ app/src/main/java/com/example/pslin_sizebook/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pslin_sizebook;
 
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,10 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
@@ -19,6 +24,7 @@ import java.io.OutputStreamWriter;
 import java.lang.reflect.Type;
 
 import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -56,14 +62,56 @@ public class MainActivity extends AppCompatActivity {
                         Record record = null;
 
                         record = new Record(name);
+
+                        recordList.add(record);
+                        adapter.notifyDataSetChanged();
+
+                        saveInFile();
                     }
                 });
-
-
             }
         });
-
-
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        readFromFile();
+
+        adapter = new ArrayAdapter<Record>(this, R.layout.record_display, recordList);
+        oldRecordsList.setAdapter(adapter);
+    }
+
+    protected void readFromFile() {
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+
+            Gson gson = new Gson();
+            //Taken from http://stackoverflow.com/questions/12384064/gson-convert-from-json-to-a-typed-arraylistt
+            //Jan/31/2017 13:41
+            Type listType = new TypeToken<ArrayList<Record>>(){}.getType();
+            recordList = gson.fromJson(in, listType);
+        } catch (FileNotFoundException e) {
+            recordList = new ArrayList<Record>();
+        }
+    }
+
+    protected void saveInFile() {
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
+
+            Gson gson = new Gson();
+            gson.toJson(recordList, out);
+            out.flush();
+
+            fos.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException();
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
 }
