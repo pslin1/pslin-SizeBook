b2c676cdecce1ed686469014558093d10540cc81
pslin1 <pslin@ualberta.ca>
2017-02-02 16:42:41 -0700

    before custom adapter creation

---
+++ app/src/main/java/com/example/pslin_sizebook/EditRecord.java
@@ -0,0 +1,13 @@
+package com.example.pslin_sizebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditRecord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_record);
+    }
+}
